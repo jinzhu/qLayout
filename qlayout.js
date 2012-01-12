d78b59f482a79933cebc0cf9b77735f774a139a8
Jinzhu <wosmvp@gmail.com>
2012-01-12 18:55:53 +0800

    drag works

--- qlayout.js
+++ qlayout.js
@@ -20,8 +20,8 @@ var qLayout = (function() {
   }
 
   function handleDragEnd(e) {
-    this.style.left = e.offsetX;
-    this.style.top  = e.offsetY;
+    this.style.left = parseInt(this.style.left) + e.offsetX;
+    this.style.top  = parseInt(this.style.top) + e.offsetY;
   }
 
   function registerAsDarggable(dom) {
@@ -33,6 +33,13 @@ var qLayout = (function() {
     dom.addEventListener('dragleave', handleDragLeave, false);
     dom.addEventListener('drop', handleDrop, false);
     dom.addEventListener('dragend', handleDragEnd, false);
+
+    var old_position    = dom.getBoundingClientRect();
+    var parent_position = document.getElementById('container').getBoundingClientRect();
+    dom.style.left      = old_position.left - parent_position.left;
+    dom.style.top       = old_position.top - parent_position.top;
+    dom.style.float     = 'none';
+    dom.style.position  = 'absolute';
   }
 
   return {
@@ -44,7 +51,7 @@ var qLayout = (function() {
 $(document).ready(function() {
   var elements = $('#container > div');
   var i;
-  for(i=0; i < elements.length; i++) {
+  for(i=elements.length-1; i > 0; i--) {
     qLayout.registerAsDarggable(elements[i]);
   }
 });
